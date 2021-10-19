f994d130f6144ab48d0ee21fd5f05356c96a70cd
Jamaril24 <jamarlopez24@g.crookcountyschools.org>
2021-10-19 09:52:38 -0700

    created JavaScript

---
+++ script.js
@@ -0,0 +1,13 @@
+
+console.log('My script is working')
+
+document. write("<h1>This is my JavaScript Demo</h1>")
+
+document. write("<h3>This was created in JavaScript not html ")
+
+alert ('The entire page was created by using only Js');
+alert ('heres a second one')
+alert ('one more')
+
+prompt ('do you like alerts')
+prompt ('why do you not like alerts')
